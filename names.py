efe7f52d786a2025904f4aa7e545dba3158bdfb0
Noah Franz WSL <noahfranz13@gmail.com>
2023-03-06 16:42:29 -0500

    fix my name

--- names.py
+++ names.py
@@ -1,6 +1,6 @@
 names = {}
 names['Mikal camarary'] = {"laptop": "Mac air 12", "phone": "blackbrry"} 
-names['Noha France'] = {"laptop": "Tandy Radio Shack 2", "phone": "Nokia windoze"} 
+names['Noah Franz'] = {"laptop": "HP Omen 16t-k000", "phone": "iPhone XR"} 
 names['Mychael Plectrum'] = {"laptop": "Gateway", "phone": "Casio flipphone"} 
 names['Aliciah Cumaray'] = {"laptop": "Alienware", "phone": "Pixelated 9"} 
 names['Mateo Laframcapitalofidaho'] = {"laptop": "Commodore64", "phone": "Samsung magallenic cloud"} 
