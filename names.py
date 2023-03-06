f83d16fdde726f83ac331e262e1d4427a3b4718f
MatthewLaframboise <md17lafr@siena.edu>
2023-03-06 16:55:11 -0500

    Fixed Name

--- names.py
+++ names.py
@@ -3,5 +3,5 @@ names['Mikal camarary'] = {"laptop": "Mac air 12", "phone": "blackbrry"}
 names['Noha France'] = {"laptop": "Tandy Radio Shack 2", "phone": "Nokia windoze"} 
 names['Mychael Plectrum'] = {"laptop": "Gateway", "phone": "Casio flipphone"} 
 names['Aliciah Cumaray'] = {"laptop": "Alienware", "phone": "Pixelated 9"} 
-names['Mateo Laframboise'] = {"laptop": "Dell", "phone": "IPhone 10 pro"} 
+names['Matthew Laframboise'] = {"laptop": "Dell", "phone": "IPhone 10 pro"} 
 names['Blayck Reavers'] = {"laptop": "Lenovo adamantium", "phone": "Ayephone 17"} 
